5b556fe0e18b7aff5b8157aa753bdba46c2dd55d
Hardy Cherry <hardyc3@gmail.com>
2014-04-08 14:08:36 -0600

    Adding new prefs for finding email in author and for setting a defect introduced date.

--- src/main/java/org/sonar/plugins/issueassign/IssueAssignPlugin.java
+++ src/main/java/org/sonar/plugins/issueassign/IssueAssignPlugin.java
@@ -23,9 +23,11 @@ import org.sonar.api.Properties;
 import org.sonar.api.Property;
 import org.sonar.api.PropertyType;
 import org.sonar.api.SonarPlugin;
+import org.sonar.plugins.issueassign.IssueAssigner;
 import org.sonar.plugins.issueassign.measures.MeasuresCollector;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -48,13 +50,34 @@ import java.util.List;
         description = "Enable or disable the Issue Assign plugin.",
         project = true,
         type = PropertyType.BOOLEAN,
-        defaultValue = "false")
+        defaultValue = "false"),
+    @Property(key = IssueAssignPlugin.PROPERTY_DEFECT_ITRODUCED_DATE,
+    	name = "Defect introduced date",
+    	description = "Any defects introduced or updated after this date are auto assigned, any defects before will be ignored. Use the format 03/22/2010 (mm/dd/yyyy)",
+    	project = true, 
+    	type = PropertyType.STRING, 
+    	defaultValue = ""),
+    @Property(key = IssueAssignPlugin.PROPERTY_EMAIL_START_CHAR,
+    	name = "Author email start character",
+    	description = "Some SCM authors may not be formatted in a way that will work with this plug in, so long as the Author contains an email address and is delimited with a start and end charater this pref can be used to find the email in the Author name.",
+    	project = true,
+    	type = PropertyType.STRING,
+    	defaultValue = ""),
+    @Property(key = IssueAssignPlugin.PROPERTY_EMAIL_END_CHAR,
+    	name = "Author email end character",
+    	description = "Some SCM authors may not be formatted in a way that will work with this plug in, so long as the Author contains an email address and is delimited with a start and end charater this pref can be used to find the email in the Author name.",
+    	project = true,
+    	type = PropertyType.STRING,
+    	defaultValue = "")
 })
 public final class IssueAssignPlugin extends SonarPlugin {
 
   public static final String PROPERTY_DEFAULT_ASSIGNEE = "default.assignee";
   public static final String PROPERTY_OVERRIDE_ASSIGNEE = "override.assignee";
   public static final String PROPERTY_ENABLED = "issueassignplugin.enabled";
+  public static final String PROPERTY_DEFECT_ITRODUCED_DATE = "defect.introduced";
+  public static final String PROPERTY_EMAIL_START_CHAR = "email.start.char";
+  public static final String PROPERTY_EMAIL_END_CHAR = "email.end.char";
 
   public List getExtensions() {
     return Arrays.asList(MeasuresCollector.class,
